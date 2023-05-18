a805e210000a706bb716f582b8a8b4c85e0134d4
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-05-18 14:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  if (decimal === 0) {
+    return '0'; // Special case for decimal 0
+  }
+
+  let binary = '';
+  while (decimal > 0) {
+    const remainder = decimal % 2;
+    binary += remainder;
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary.split('').reverse().join('');
 }
 
 module.exports = threeSum;
